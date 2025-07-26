0c6be3189b90bbdd7f58677aa87d84c50dd9af22
eulucaslim <lucaslimacnt14@gmail.com>
2025-07-25 20:43:56 -0400

    Add shadcn in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-full bg-slate-500">
-      <h2 className='text-4xl text-red-400'>Hello World</h2>
+    <div className="min-h-screen min-w-full bg-black">
+      <h2 className='text-4xl text-white'>Hello World</h2>
+      <div className="flex justify-center items-center">
+        <Button variant="secondary">Button</Button>
+      </div>
     </div>
   );
 }
